5a1d72be21602087265b0012ec7f4b89c623a84d
letruxux <mrgianfranco483@gmail.com>
2026-03-15 19:50:03 +0100

    fix: fix weird negative timestamps

--- src/utils.ts
+++ src/utils.ts
@@ -12,6 +12,10 @@ export function useTemplate(template: string, data: Record<string, string>) {
   return template.replaceAll(/{{(\w+)}}/g, (_, key) => data[key] ?? "");
 }
 
+function positiveOrZero(num: number) {
+  return num > 0 ? num : 0;
+}
+
 export function timePassedToString(ms: number) {
   const totalSeconds = Math.floor(ms / 1000);
 
@@ -20,12 +24,14 @@ export function timePassedToString(ms: number) {
   const seconds = totalSeconds % 60;
 
   if (hours > 0) {
-    return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds
+    return `${hours}:${positiveOrZero(minutes).toString().padStart(2, "0")}:${positiveOrZero(
+      seconds,
+    )
       .toString()
       .padStart(2, "0")}`;
   }
 
-  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+  return `${positiveOrZero(minutes)}:${positiveOrZero(seconds).toString().padStart(2, "0")}`;
 }
 
 export function calculateTimer(str: string) {
@@ -34,7 +40,7 @@ export function calculateTimer(str: string) {
   const timer = timerMatch
     ? Math.floor(
         (+(timerMatch[1] ?? 0) * 60 + +(timerMatch[2] ?? 0)) /
-          env.TIMER_UPDATE_INTERVAL_SECONDS
+          env.TIMER_UPDATE_INTERVAL_SECONDS,
       ) * env.TIMER_UPDATE_INTERVAL_SECONDS
     : undefined;
 
